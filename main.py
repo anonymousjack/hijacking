bbabb152a21f6a0c1e8aa32bad62fe48a4bdfcf7
anonymousjack <jackjia@umich.edu>
2019-06-28 17:49:00 -0700

    upload entire dataset

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import sys
-sys.path.append('./')
+import os
+sys.path.append(os.path.abspath('./'))
 from attack.attack_v2 import attack_video
 from collections import deque
 import numpy as np
-import os
 from tqdm import tqdm
 from attack.attack_v2 import cal_success_rate
 
 if __name__ == "__main__":
     videos_info = [
-            #                            [left, top, right, bottom]
-            #('move-in-zoomed/01.mp4', 3, [188, 284, 247, 309], None),
-            #('move-in-zoomed/02.mp4', 3, [195, 233, 289, 264], None),
-            #('move-in-zoomed/03.mp4', 3, [238, 194, 319, 256], None),
+            #  video file name, attack start frame,  patch coords:[left, top, right, bottom]
+            ('move-in-zoomed/01.mp4', 3, [188, 284, 247, 309], None),
+            ('move-in-zoomed/02.mp4', 3, [195, 233, 289, 264], None),
+            ('move-in-zoomed/03.mp4', 3, [238, 194, 319, 256], None),
             ('move-in-zoomed/04.mp4', 3, [229, 231, 305, 267], None)
-            #('move-in-zoomed/05.mp4', 3, [202, 172, 277, 222], None),
-            #('move-in-zoomed/06.mp4', 3, [236, 230, 334, 298], None),
-            #('move-in-zoomed/07.mp4', 3, [195, 203, 252, 260], None),
-            #('move-in-zoomed/08.mp4', 3, [136, 193, 247, 280], None),
-            #('move-in-zoomed/09.mp4', 3, [246, 210, 373, 340], None),
-            #('move-in-zoomed/10.mp4', 5, [196, 205, 300, 287], None),
-            #('move-out-zoomed/01.mp4', 3, [192, 213, 310, 273], [5, 0, 5, 0]),
-            #('move-out-zoomed/02.mp4', 3, [143, 222, 303, 302], [5, 0, 5, 0]),
-            #('move-out-zoomed/03.mp4', 3, [158, 192, 300, 283], [-5, 0, -5, 0]),
-            #('move-out-zoomed/04.mp4', 3, [154, 230, 281, 289], [-5, 0, -5, 0]),
-            #('move-out-zoomed/05.mp4', 3, [194, 167, 297, 249], [5, 0, 5, 0]),
-            #('move-out-zoomed/06.mp4', 3, [174, 166, 326, 280], [-5, 0, -5, 0]),
-            #('move-out-zoomed/07.mp4', 3, [182, 211, 304, 271], [5, 0, 5, 0]),
-            #('move-out-zoomed/08.mp4', 3, [100, 131, 304, 307], [-5, 0, -5, 0]),
-            #('move-out-zoomed/09.mp4', 3, [144, 188, 293, 310], [-5, 0, -5, 0]),
-            #('move-out-zoomed/10.mp4', 3, [171, 159, 264, 238], [5, 0, 5, 0]),
+            ('move-in-zoomed/05.mp4', 3, [202, 172, 277, 222], None),
+            ('move-in-zoomed/06.mp4', 3, [236, 230, 334, 298], None),
+            ('move-in-zoomed/07.mp4', 3, [195, 203, 252, 260], None),
+            ('move-in-zoomed/08.mp4', 3, [136, 193, 247, 280], None),
+            ('move-in-zoomed/09.mp4', 3, [246, 210, 373, 340], None),
+            ('move-in-zoomed/10.mp4', 5, [196, 205, 300, 287], None),
+            ('move-out-zoomed/01.mp4', 3, [192, 213, 310, 273], [5, 0, 5, 0]),
+            ('move-out-zoomed/02.mp4', 3, [143, 222, 303, 302], [5, 0, 5, 0]),
+            ('move-out-zoomed/03.mp4', 3, [158, 192, 300, 283], [-5, 0, -5, 0]),
+            ('move-out-zoomed/04.mp4', 3, [154, 230, 281, 289], [-5, 0, -5, 0]),
+            ('move-out-zoomed/05.mp4', 3, [194, 167, 297, 249], [5, 0, 5, 0]),
+            ('move-out-zoomed/06.mp4', 3, [174, 166, 326, 280], [-5, 0, -5, 0]),
+            ('move-out-zoomed/07.mp4', 3, [182, 211, 304, 271], [5, 0, 5, 0]),
+            ('move-out-zoomed/08.mp4', 3, [100, 131, 304, 307], [-5, 0, -5, 0]),
+            ('move-out-zoomed/09.mp4', 3, [144, 188, 293, 310], [-5, 0, -5, 0]),
+            ('move-out-zoomed/10.mp4', 3, [171, 159, 264, 238], [5, 0, 5, 0]),
     ]
 
     dir_path = './data/'
